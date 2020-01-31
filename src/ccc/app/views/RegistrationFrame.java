99a4680d0af355df4c4308091c10be493f16f351
Alamin Hossain <hossainalamin72@gmail.com>
2020-01-31 22:40:39 +0600

    adding ignoring file

--- src/ccc/app/views/RegistrationFrame.java
+++ src/ccc/app/views/RegistrationFrame.java
@@ -6,16 +6,22 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.FlowLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.Component;
 
 public class RegistrationFrame extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -38,45 +44,19 @@ public class RegistrationFrame extends JFrame {
 	 */
 	public RegistrationFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 621, 590);
+		setBounds(100, 100, 558, 387);
 		contentPane = new JPanel();
+		contentPane.setAlignmentY(Component.TOP_ALIGNMENT);
+		contentPane.setAlignmentX(Component.LEFT_ALIGNMENT);
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panelRegisHeader = new JPanel();
-		panelRegisHeader.setBounds(0, 0, 605, 54);
-		panelRegisHeader.setBackground(new Color(153, 153, 255));
-		contentPane.add(panelRegisHeader);
-		panelRegisHeader.setLayout(null);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(306, 14, 1, 1);
-		contentPane.add(panel_1);
-		panel_1.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("First Name");
-		lblNewLabel.setBounds(10, 0, 91, 28);
-		panel_1.add(lblNewLabel);
-		
-		textField = new JTextField();
-		textField.setBounds(111, 4, 112, 20);
-		panel_1.add(textField);
-		textField.setColumns(10);
-		
-		JPanel panel = new JPanel();
-		panel.setBounds(312, 14, 1, 1);
-		panel.setLayout(null);
-		contentPane.add(panel);
-		
-		JLabel lblLastName = new JLabel("Last Name");
-		lblLastName.setBounds(56, 0, 50, 28);
-		panel.add(lblLastName);
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(127, 4, 103, 20);
-		panel.add(textField_1);
+		JPanel panelRegisterHeader = new JPanel();
+		panelRegisterHeader.setBackground(new Color(64, 224, 208));
+		FlowLayout fl_panelRegisterHeader = (FlowLayout) panelRegisterHeader.getLayout();
+		fl_panelRegisterHeader.setVgap(40);
+		contentPane.add(panelRegisterHeader, BorderLayout.NORTH);
 	}
-
 }
